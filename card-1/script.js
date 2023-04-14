8cb2425eea0732bc84c3c4b929585cbf3927e817
Cícero Santos <cicero.dsantos@outlook.com.br>
2023-04-14 17:02:53 -0300

    update photo

--- card-1/script.js
+++ card-1/script.js
@@ -4,10 +4,10 @@ function toggleMode(){
   
   if(html.classList.contains('light')){
     html.classList.remove('light');
-    srcImage.setAttribute("src", "./assets/veo.jpg");
+    srcImage.setAttribute("src", "./assets/veronicadiastt.jpg");
   }else{
     html.classList.add('light');
-    srcImage.setAttribute("src", "./assets/veo.jpg");
+    srcImage.setAttribute("src", "./assets/veronicadiastt.jpg");
      
   }
   
