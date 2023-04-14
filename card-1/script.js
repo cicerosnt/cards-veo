dc05bfe79af13726610ed389224d02d5fc0f4d27
Cícero Santos <cicero.dsantos@outlook.com.br>
2023-04-14 14:22:07 -0300

    first commit

---
+++ card-1/script.js
@@ -0,0 +1,15 @@
+function toggleMode(){
+  const html = document.documentElement;
+  const srcImage = document.querySelector("#imgProfile");
+  
+  if(html.classList.contains('light')){
+    html.classList.remove('light');
+    srcImage.setAttribute("src", "./assets/veo.jpg");
+  }else{
+    html.classList.add('light');
+    srcImage.setAttribute("src", "./assets/veo.jpg");
+     
+  }
+  
+  //html.classList.toggle('light')
+}
